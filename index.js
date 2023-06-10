bd8897b9371e7bd50c826935116cb3530896a7b9
mendesffxj <136197914+mendesffxj@users.noreply.github.com>
2023-06-10 17:12:22 -0300

    Subir arquivos do meu site

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+if( getCookie("cadastro") == "true" ) {
+    window.location.href = "Mines.html";
+} */
+
+const buttonNextPage = document.getElementById("next-page");
+
+window.addEventListener("load", (event) => {
+    setTimeout(() => {
+        buttonNextPage.disabled = false;
+    }, 35000);
+});
+
+function pageFree() {
+    window.open("Mines.html", "_self");
+}
